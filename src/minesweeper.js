1eabfa55f4213966a393240ec562d176c66efce9
Kyle Scott <kyle.j.scott@hotmail.com>
2018-09-14 17:59:35 +0100

    inital commit

---
+++ src/minesweeper.js
@@ -0,0 +1,14 @@
+const blankLine = '   |   |   ';
+const guessLine = ' 1 |   |   ';
+const bombLine =  '   | B |   ';
+
+
+console.log('This is what a blank board looks like:');
+console.log(blankLine);
+console.log(blankLine);
+console.log(blankLine);
+
+console.log('This is what a board looks like with a bomb and a guess:');
+console.log(guessLine);
+console.log(bombLine);
+console.log(blankLine);
